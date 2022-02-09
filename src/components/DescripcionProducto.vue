c69b165db82aadffe6a1893f24b01ce2ce1e73b9
Richard Montenegro <1903023@alu.murciaeduca.es>
2022-02-09 23:51:09 +0100

    anadido video y descripcion

--- src/components/DescripcionProducto.vue
+++ src/components/DescripcionProducto.vue
@@ -2,11 +2,11 @@
   <section id="producto-descripcion">
       <h1>DESCRIPCIÓN DEL PRODUCTO</h1>
       <ul>
-          <li>HUAWEI MateBook X es pequeño pero poderoso; su procesador de hasta 7ª generación Intel® Core™ i7 proporciona potencia suficiente tanto para trabajo como para entretenimiento. </li>
-          <li>La ingeniería aeroespacial permite al HUAWEI MateBook X permanecer frío gracias a sus materiales de cambio de fase (usados en la industria espacial), lo que significa que el sobrecalentamiento es una cosa del pasado.</li>
-          <li>El botón de encendido dos en uno del HUAWEI MateBook X y su sensor de reconocimiento por huella dactilar te ayudan a arrancarlo de forma rápida y segura para funcionar a la velocidad de tu vida.</li>
-          <li>Seguridad total. Hoy en día utilizamos los dispositivos electrónicos para todo tipo de cosas; desde transacciones bancarias hasta el almacenamiento de documentos importantes. Por ello, la seguridad nunca está de más. El portátil Huawei está equipado con un sensor de huella dactilar para que sólo tú puedas iniciar sesión y utilizarlo. </li>
-          <li>Memoria ultrarrápida. Con el disco duro de 256 GB SSD podrás arrancar tu Windows 10 como un verdadero tiro. En un abrir y cerrar de ojos tendrás listo tu portátil.  </li>
+          <li>Hecho por y para jugadores profesionales: Diseñado con los principales profesionales de deportes electrónicos; diseñado para ganar; la búsqueda del mayor nivel de rendimiento es nuestro objetivo</li>
+          <li>Ingeniería ultraligera: Con menos de 63 gramos, con un rediseño minismalista que logra una reducción de peso de casi un 25% en comparación con el ratón inalámbrico PRO estándar</li>
+          <li>LIGHTSPEED inalámbrico: Con un rendimiento inalámbrico USB de nivel profesional, LIGHTSPEED proporciona una conexión sólida y rápida con una velocidad de transmisión de 1 ms y sin cables</li>
+          <li>Sensor HERO 25K: Este ratón para gaming Pro cuenta con el sensor HERO para mayor precisión, rendimiento y eficiencia durante el juego; supera los 400 IPS y ofrece un seguimiento de 25600 DPI</li>
+          <li>Deslizamiento suave: Los grandes pies de PTFE sin aditivos de este ratón para videojuegos Logitech G ofrecen un deslizamiento suave para una conexión pura y fluida durante el juego</li>
       </ul>
   </section>
 </template>
--- src/components/VideoProducto.vue
+++ src/components/VideoProducto.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>VIDEO</h1>
+  <iframe width="840" height="472" src="https://www.youtube.com/embed/z5YelNncnMY" 
+  title="YouTube video player" frameborder="0" allow="accelerometer; 
+  autoplay; clipboard-write; encrypted-media; gyroscope; 
+  picture-in-picture" allowfullscreen></iframe>
 </template>
 
 <script>
